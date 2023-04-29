e69933f82e8ffaaed4a4c09d3b0a5b42616d308e
Eric <eric@baochi.xin>
2023-04-29 12:07:56 +0800

    fix

---
+++ pages/api/hello.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Midjourney } from "midjourney";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ResponseError } from "../../interfaces";
+import { Readable } from "stream";
+const client = new Midjourney(
+  <string>process.env.SERVER_ID,
+  <string>process.env.CHANNEL_ID,
+  <string>process.env.SALAI_TOKEN
+);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { prompt } = req.body;
+  const stream = new Readable({
+    read() {
+      this.push("hello");
+      setTimeout(() => {
+        this.push("world");
+        this.push(null);
+      }, 1000);
+    },
+  });
+  stream.pipe(res);
+}
