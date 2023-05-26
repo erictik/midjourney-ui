09ee3b37beed4b7e6f38ef06f6386be19b9d7123
Eric <eric@baochi.xin>
2023-05-26 11:29:06 +0800

    add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
+import type { AppProps } from "next/app";
+import MainLayout from "../layouts/main";
+import "../public/antd.min.css";
+import "../styles/globals.css";
+import withTheme from "../theme";
+import { Analytics } from "@vercel/analytics/react";
 
-import type { AppProps } from 'next/app';
-import MainLayout from '../layouts/main'
-import '../public/antd.min.css';
-import '../styles/globals.css'
-import withTheme from '../theme';
 export default function App({ Component, pageProps }: AppProps) {
   return withTheme(
-    MainLayout( <Component {...pageProps} />)
+    MainLayout(
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+    )
   );
 }
